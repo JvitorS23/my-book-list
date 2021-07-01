38ac6273c59e723dc8f261ae8063cac5902102fc
José Vitor Santos Silva <jose.silva@dcomp.ufs.br>
2021-07-01 20:05:29 -0300

    Landing page creation

--- books_app/books_app/settings.py
+++ books_app/books_app/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'core', 'static'),
+)
